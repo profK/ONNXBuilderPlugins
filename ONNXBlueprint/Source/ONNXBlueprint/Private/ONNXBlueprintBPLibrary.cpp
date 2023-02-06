1e7f409c943064837bbd893589444fddd0d0b74d
Rose <hassanarshad2011@gmail.com>
2023-02-06 16:28:55 -0500

    Cleaning up

--- ONNXBlueprint/Source/ONNXBlueprint/Private/ONNXBlueprintBPLibrary.cpp
+++ ONNXBlueprint/Source/ONNXBlueprint/Private/ONNXBlueprintBPLibrary.cpp
@@ -39,16 +39,26 @@ TArray<float> UONNXBlueprintBPLibrary::ExtractWeights(FONNXModel model, FString&
 	TArray<float> TWeights;
 
 	TWeights.Append(weights, FONNXBuilderCModule::GetMLPWeightCount(model.Model));
+
+	// Demo
+	//FString ArrayString;
+	//for (float Value : TWeights)
+	//{
+	//	ArrayString += FString::SanitizeFloat(Value) + TEXT(", ");
+	//}
+	//ArrayString = ArrayString.LeftChop(2);
+	//UE_LOG(LogTemp, Log, TEXT("MyArray: %s"), *ArrayString);
+
 	return TWeights;
 }
 
-void UONNXBlueprintBPLibrary::SetWeights(FONNXModel model, TArray<float> weights, FString& Report)
+void UONNXBlueprintBPLibrary::SetWeights(FONNXModel& model, TArray<float> weights, FString& Report)
 {
 	FONNXBuilderCModule::SetMLPWeights(model.Model, weights.GetData());
 }
 
 
-TArray<uint8> UONNXBlueprintBPLibrary::GetByteArray(FONNXModel model, FString SaveDirectory, FString& Report)
+TArray<uint8> UONNXBlueprintBPLibrary::GetByteArray(FONNXModel model, FString& Report)
 {
 	uint8_t* bArray = FONNXBuilderCModule::GetMLPByteArray(model.Model);
 	TArray<uint8> TBytes;
@@ -57,87 +67,5 @@ TArray<uint8> UONNXBlueprintBPLibrary::GetByteArray(FONNXModel model, FString Sa
 	TBytes.Append(bArray, onnxSize);
 
 	return TBytes;
-
-	//FString fileByteString((char*)bArray);
-	//FString temp_file = FPaths::CreateTempFilename(TEXT(""), TEXT(""), nullptr);
-	//FFileHelper::SaveStringToFile(fileByteString, *temp_file);
-
-	//TArray<uint8> model_array;
-	//FFileHelper::LoadFileToArray(model_array, *temp_file);
-	//IFileManager::Get().Delete(*temp_file);
-
-	//FONNXBuilderCModule::WriteMLPFile(model.Model);
-
-	//SaveDirectory += "mlp1.onnx";
-	//const FString InModelFilePath = SaveDirectory;
-
-	//FString ModelFullFilePath = IFileManager::Get().ConvertToAbsolutePathForExternalAppForRead(*InModelFilePath);
-	//TArray<uint8> ModelReadFromFileInBytes;
-	//if (FFileHelper::LoadFileToArray(ModelReadFromFileInBytes, *ModelFullFilePath))
-	//{
-	//	Report = FString::Printf(TEXT("Read bytes from file %s"), *InModelFilePath);
-	//}
-	//else
-	//{
-	//	Report = FString::Printf(TEXT("file not read!"));
-	//}
-
-	//FString fileByteString((char*)ModelReadFromFileInBytes.GetData());
-
-	//Report = FString::Printf(TEXT("Read bytes from file %s"), *fileByteString);
-
-	//return model_array;
-
-	// return model_array;
-}
-
-void UONNXBlueprintBPLibrary::CompareBytes(TArray<uint8> fileByte, TArray<uint8> arrayByte, FString& Report)
-{
-	// Passing
-	FString fileByteString((char*)fileByte.GetData());
-	FString arrayByteString((char*)arrayByte.GetData());
-
-	//FString fileByteString((char*)bArray);
-	FString temp_file = FPaths::CreateTempFilename(TEXT(""), TEXT(""), nullptr);
-	FFileHelper::SaveStringToFile(fileByteString, *temp_file);
-
-	TArray<uint8> model_array;
-	FFileHelper::LoadFileToArray(model_array, *temp_file);
-	IFileManager::Get().Delete(*temp_file);
-
-	Report = FString::Printf(TEXT("Compare Bytes %s"), model_array == fileByte, fileByteString == arrayByteString);
-
-	
-	//uint8* file = fileByte.GetData();
-	//uint8* arr = arrayByte.GetData();
-
-	//FString wrongs = "";
-
-	//for (int i = 0; i < fileByte.Num(); i++)
-	//{
-	//	if (fileByte[i] != arrayByte[i])
-	//	{
-	//		wrongs = wrongs + (FString)i + " ";
-	//	}
-	//}
-
-	//int onnxSize = 874;
-	//TArray<uint8> TBytes2;
-	//TBytes2.Append(arrayByte.GetData(), onnxSize);
-
-	// Report = FString::Printf(TEXT("FString Model %s"), *arrayByteString);
-
-	//TArray<TCHAR> model_char_array;
-	//fileByteString.GetCharArray(model_char_array);
-	//TArray<uint8> model_uint8_array;
-	//model_uint8_array.SetNumUninitialized(model_char_array.Num());
-	//FMemory::Memcpy(model_uint8_array.GetData(), model_char_array.GetData(), model_char_array.Num());
-	
-	//TArray<uint8> model_data;
-	//int32 ModelStringLength = fileByteString.Len();
-	//model_data.SetNumUninitialized(ModelStringLength);
-	//FMemory::Memcpy(model_data.GetData(), TCHAR_TO_UTF8(*fileByteString), ModelStringLength);
-
-	// FONNXBuilderCModule::WriteMLPFile(model.Model);
 }
 
--- ONNXBlueprint/Source/ONNXBlueprint/Public/ONNXBlueprintBPLibrary.h
+++ ONNXBlueprint/Source/ONNXBlueprint/Public/ONNXBlueprintBPLibrary.h
@@ -54,12 +54,12 @@ class UONNXBlueprintBPLibrary : public UBlueprintFunctionLibrary
 	static TArray<float> ExtractWeights(FONNXModel model, FString& Report);
 
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "SetWeights", Keywords = "Set Weight ONNXBlueprint NNI MLP"), Category = "ONNXBlueprint")
-	static void SetWeights(FONNXModel model, TArray<float> weights, FString& Report);
+	static void SetWeights(FONNXModel& model, TArray<float> weights, FString& Report);
 
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "GetByteArray", Keywords = "Get Byte Array ONNXBlueprint NNI MLP"), Category = "ONNXBlueprint")
-	static TArray<uint8> GetByteArray(FONNXModel model, FString SaveDirectory, FString& Report);
+	static TArray<uint8> GetByteArray(FONNXModel model, FString& Report);
 
-	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Compare Bytes", Keywords = "Compare Bytes ONNXBlueprint NNI MLP"), Category = "ONNXBlueprint")
-	static void CompareBytes(TArray<uint8> fileByte, TArray<uint8> arrayByte, FString& Report);
+	//UFUNCTION(BlueprintCallable, meta = (DisplayName = "Compare Bytes", Keywords = "Compare Bytes ONNXBlueprint NNI MLP"), Category = "ONNXBlueprint")
+	//static void CompareBytes(TArray<uint8> fileByte, TArray<uint8> arrayByte, FString& Report);
 
 };
