b1ebc922d402ded754896f88650371f12539918b
professorK <jeffpk@gmail.com>
2024-10-08 18:47:33 -0400

    working better
    Still needs  logic

---
+++ NNEBPLib/Source/NNEBPLib/Private/NNEBPLibBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NNEBPLibBPLibrary.h"
+#include "NNEBPLib.h"
+
+UNNEBPLibBPLibrary::UNNEBPLibBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UNNEBPLibBPLibrary::NNEBPLibSampleFunction(float Param)
+{
+	return -1;
+}
+
